39236efa7901197037824b3c48eea979096ef091
Fr4ctlize <titaniumshade22@outlook.com>
2020-10-19 13:02:49 +0200

    added ;

--- ej/scripts/login.js
+++ ej/scripts/login.js
@@ -1,16 +1,16 @@
 
 function validateLoginForm() {
-    const email = document.forms['login']['user'].value
-    const password = document.forms['login']['password'].value
-    const userCredentials = getCookie(email)
+    const email = document.forms['login']['user'].value;
+    const password = document.forms['login']['password'].value;
+    const userCredentials = getCookie(email);
     if (userCredentials !== '') {
         if (password === userCredentials.split('=')[1]) {
-            window.location.href = 'index.html'
+            window.location.href = 'index.html';
         }
     } else {
-        document.querySelectorAll('#login-error').style.display = 'flex'
+        document.querySelectorAll('#login-error').style.display = 'flex';
     }
-    return false
+    return false;
 }
 
 function getCookie(cname) {
