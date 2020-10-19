bcabe5616ddeb15e9585fac4ca287f7fdce07773
burned301 <pablosky301@gmail.com>
2020-10-19 13:34:51 +0200

    Working on register validation

--- ej/scripts/register.js
+++ ej/scripts/register.js
@@ -6,15 +6,35 @@ document.addEventListener('DOMContentLoaded', () => {
 function register(){
 
     document.querySelectorAll('.error').forEach(error =>{
-        console.log(error);
         error.innerHTML = "";
     });
 
-
-
     const form = document.forms['register'];
+    const mandatoryField = 'Campo obligatorio';
 
     const username = form['username'];
+    const nia = form['nia'];
+    const pswd = form['password'];
+    const name = form['name'];
+    const email = form['email'];
+    //const
+
+
+
+    /*
+    if(!username.checkValidity()) document.querySelector('#username-error').innerHTML = username.validationMessage;
+    if(|username.c)
+    */
+
+
+    document.querySelectorAll('input').forEach( input =>{
+        if(!input.checkValidity()) {
+            const name = input.getAttribute('name');
+            let query = `#${name}-error`;
+            document.querySelector(query) = input.validationMessage;
+        }
+    })
+
     return false;
 }
 
