d077397cc0cf0cb6533cb9ad6dd0889d50787b2f
burned301 <pablosky301@gmail.com>
2020-10-20 13:40:42 +0200

    Working on register cookis

--- ej/scripts/register.js
+++ ej/scripts/register.js
@@ -6,19 +6,30 @@ document.addEventListener('DOMContentLoaded', () => {
 //pattern="(100)(\d){6}"
 
 function register(){
-
+    const valid = true;
+    let cookie = '';
     document.querySelectorAll('.error').forEach(error =>{
         error.innerHTML = "";
     });
 
     document.querySelectorAll('input').forEach( input =>{
+        const name = input.getAttribute('name');
+        cookie += `${name}=${input.value};`;
         if(!input.checkValidity()) {
-            const name = input.getAttribute('name');
             document.querySelector(`#${name}-error`).innerHTML = input.validationMessage;
         }
     })
 
+    if(valid) setCookie(cookie,1);
     return false;
+}
+
+function setCookie(cookie, exdays) {
+    alert(cookie);
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cookie + expires + ";path=/";
 }
 
 
